b8b3ea63e52a76eaa2bb88e4a8653ea699c98100
Zoe Koh <eden11@protonmail.com>
2020-11-06 22:24:49 +1100

    Fixed the buttons and added more games

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,20 +4,21 @@ import { FaCartPlus } from "react-icons/fa";
 
 const FUN = "Fun";
 const ADVENTURE = "Adventure";
+const PUZZLES = "Puzzles";
 
 function Products({ setCart, cart }) {
   const [products] = useState([
     {
       // category: ADVENTURE,
       name: "Cyberpunk 2077",
-      cost: 28.95,
+      cost: "Coming Soon",
       image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1rft.jpg",
       link: "https://www.cyberpunk.net/us/en/pre-order",
     },
     {
       // category: FUN,
       name: "Fall Guys",
-      cost: 28.95,
+      cost: "$28.95",
       image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2fq7.jpg",
       link:
         "https://store.steampowered.com/app/1097150/Fall_Guys_Ultimate_Knockout/",
@@ -25,9 +26,31 @@ function Products({ setCart, cart }) {
     {
       // category: FUN,
       name: "Among Us",
-      cost: 28.95,
+      cost: "$7.5",
       image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1uaf.jpg",
       link: "https://store.steampowered.com/app/945360/Among_Us/",
+    },
+    {
+      // category: PUZZLES,
+      name: "A Monster's Expedition",
+      cost: "$28.95",
+      image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2eop.jpg",
+      link: "https://store.steampowered.com/app/1052990/A_Monsters_Expedition/",
+    },
+    {
+      // category: ADVENTURE,
+      name: "Halo Infinite",
+      cost: "Coming Soon",
+      image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2dto.jpg",
+      link: "https://store.steampowered.com/app/1240440/Halo_Infinite/",
+    },
+    {
+      // category: FUN,
+      name: "Rocket League",
+      cost: "Price Unknown",
+      image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2hnd.jpg",
+      link:
+        "https://store.steampowered.com/app/252950/Rocket_League/?curator_clanid=11855704",
     },
   ]);
   const addToCart = (product) => {
@@ -56,18 +79,14 @@ function Products({ setCart, cart }) {
             <li key={index}>
               <h2>{product.name}</h2>
               <div className="space3"></div>
-              <img src={product.image} alt="Product Image" />
+              <img src={product.image} alt={product.name} />
               <div className="space"></div>
-              <div className="buy">
-                <button
-                  onClick={() => (window.location.href = products[0].link)}
-                >
-                  CodePen
+              <div className="thiscard">
+                <button onClick={() => (window.location.href = product.link)}>
+                  Buy
                 </button>
-                {/* <p onClick={products.link}>buy</p>
-                <button onClick="">Buy</button> */}
               </div>
-              <h4>${product.cost}</h4>
+              <h4>{product.cost}</h4>
               <div className="cart-container">
                 <div className="space2"></div>
                 <div className="anotheridk">
